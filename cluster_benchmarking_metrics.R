c6e19edc0608c3fc497d42b2aa9f4fb5936dd1e7
fmartinez-tgu <98175102+fmartinez-tgu@users.noreply.github.com>
2026-04-27 12:47:37 +0200

    New files and corrections

--- cluster_benchmarking_metrics.R
+++ cluster_benchmarking_metrics.R
@@ -1,38 +1,39 @@
-### RAND-INDEX
+# This script calculates the Adjusted Rand Index (ARI) and Precision/Recall metrics
+# for comparing two clustering results: a gold standard (GS) and a method (M2)
 
-# Install mclust if you haven't: install.packages("mclust")
+### ADJUSTED RAND-INDEX
+
 library(tidyverse)
 library(mclust)
  
-# 1. Load data
-gs_df <- read.csv("/Users/fran/Downloads/zvalencia_multifasta_h37rv.clusters_10.tsv", sep = "\t")
-m2_df <- read.csv("/Users/fran/Downloads/valencia_dataset.masked10.clusters_10.tsv", sep = "\t")
+# Let's load the data
+gs_df <- read.csv("clusters_from_multifasta_GS.tsv", sep = "\t") # GS clusters
+m2_df <- read.csv("clusters_from_ushersampled_phylogeny_M2.tsv", sep = "\t") # M2 clusters
 
-# 2. Reshape to long format (Sample | ClusterID)
+# We convert the cluster assignments into a long format where each sample has its own row
 gs_long <- gs_df %>% separate_rows(Samples, sep = ",")
 m2_long <- m2_df %>% separate_rows(Samples, sep = ",")
 
-# 3. Create a master list of all samples
+# Now we create a combined dataframe to compare the cluster assignments for each sample
 all_samples <- unique(c(gs_long$Samples, m2_long$Samples))
 comparison_df <- data.frame(Sample = all_samples)
 
-# 4. Join the cluster IDs
+# We join the cluster assignments from both GS and M2 to this combined dataframe
 comparison_df <- comparison_df %>%
   left_join(gs_long, by = c("Sample" = "Samples")) %>%
   rename(GS_Cluster = Cluster) %>%
   left_join(m2_long, by = c("Sample" = "Samples")) %>%
   rename(M2_Cluster = Cluster)
 
-# 5. Handle singletons (NAs)
-# Samples not in a cluster are unique entities. 
-# We assign them a unique dummy ID so they aren't grouped together.
+# Some samples might be missing from either GS or M2, we can assign them to a unique cluster 
+# (e.g., "S_GS_X" or "S_M2_X") to ensure they are treated as singletons in the adjusted Rand-index calculation
 comparison_df <- comparison_df %>%
   mutate(
     GS_Cluster = ifelse(is.na(GS_Cluster), paste0("S_GS_", row_number()), GS_Cluster),
     M2_Cluster = ifelse(is.na(M2_Cluster), paste0("S_M2_", row_number()), M2_Cluster)
   )
 
-# 6. Calculate ARI
+# Now we can calculate the Adjusted Rand Index
 ari_score <- adjustedRandIndex(comparison_df$GS_Cluster, comparison_df$M2_Cluster)
 
 cat(paste("Adjusted Rand Index:", round(ari_score, 4)))
@@ -43,27 +44,27 @@ cat(paste("Adjusted Rand Index:", round(ari_score, 4)))
 
 library(tidyverse)
 
-# 1. Load Data
-gs_raw <- read.csv("/Users/fran/Downloads/zvalencia_multifasta_h37rv.clusters_10.tsv", sep = "\t")
-m2_raw <- read.csv("/Users/fran/Downloads/valencia_dataset.masked10.clusters_10.tsv", sep = "\t")
+# Let's load the data again (or you can reuse the already loaded dataframes)
+gs_raw <- read.csv("clusters_from_multifasta_GS.tsv", sep = "\t")
+m2_raw <- read.csv("clusters_from_ushersampled_phylogeny_M2.tsv", sep = "\t")
 
-# Helper: calculate n*(n-1)/2
+# Function to calculate the number of pairs in a cluster of size n
 count_pairs <- function(n) { (n * (n - 1)) / 2 }
 
-# 2. Total pairs that SHOULD exist (Gold Standard)
+# Total pairs in GS
 gs_total_pairs <- gs_raw %>%
   mutate(n = str_count(Samples, ",") + 1) %>%
   summarise(total = sum(count_pairs(n))) %>%
   pull(total)
 
-# 3. Total pairs Method 2 CLAIMED exist
+# Total pairs in M2
 m2_total_pairs <- m2_raw %>%
   mutate(n = str_count(Samples, ",") + 1) %>%
   summarise(total = sum(count_pairs(n))) %>%
   pull(total)
 
-# 4. Calculate True Positives (TP)
-# We find the intersection: how many samples from GS_Cluster_A are also in M2_Cluster_B?
+# To calculate true positives, we need to find how many pairs of samples are clustered together in both GS and M2.
+
 gs_long <- gs_raw %>% separate_rows(Samples, sep = ",")
 m2_long <- m2_raw %>% separate_rows(Samples, sep = ",")
 
@@ -76,12 +77,11 @@ tp <- inner_join(gs_long, m2_long, by = "Samples") %>%
   summarise(grand_tp = sum(sum_tp)) %>%
   pull(grand_tp)
 
-# 5. Metrics
+# Now we can calculate precision and recall
 precision <- tp / m2_total_pairs
 recall <- tp / gs_total_pairs
-f1 <- 2 * (precision * recall) / (precision + recall)
 
-cat(sprintf("Precision: %.4f\nRecall:    %.4f\nF1-Score:  %.4f\n", precision, recall, f1))
+cat(sprintf("Precision: %.4f\nRecall:    %.4f", precision, recall))
 
 
 
